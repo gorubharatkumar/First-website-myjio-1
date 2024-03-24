696c2f51a3930c974915813d42912507c89e4fd7
gorubharatkumar <158815076+gorubharatkumar@users.noreply.github.com>
2024-03-25 03:55:31 +0530

    Add files via upload

---
+++ tune.js
@@ -0,0 +1,48 @@
+function goru(){
+    document.getElementById("poli").style.display="none";
+    document.getElementById("joke").style.display="block";
+    document.getElementById("check").style.display="block";
+    document.getElementById("check").style.borderBottom="2px solid rgb(176, 173, 173)";
+    document.getElementById("f6").style.border="none";
+
+
+}
+
+function kumar(){
+    document.getElementById("poli").style.display="block";
+    document.getElementById("joke").style.display="none";
+    document.getElementById("check").style.display="none";
+    
+    document.getElementById("f6").style.borderBottom="2px solid rgb(176, 173, 173)";
+    
+
+
+}
+function siva(){
+
+    document.getElementById("bodylogo").style.display="none";
+    document.getElementById("totalroy").style.display="block";
+
+    document.getElementById("marder").style.display="none";
+
+    document.getElementById("vinay").style.display="block"
+    
+  
+}
+function eswar(){
+    document.getElementById("vinay").style.display="none"
+
+    document.getElementById("marder").style.display="block";
+    document.getElementById("bodylogo").style.display="block";
+    document.getElementById("totalroy").style.display="none";
+
+}
+
+
+
+
+
+
+
+
+
